c5392e061eda0bebd539f0b42dbaecda53c004ab
Olubunmi Alegbeleye <olubunmialegbeleye@users.noreply.github.com>
2020-10-26 10:07:06 +0100

    Code the SigninActivity to check if the db contains entry

--- app/src/main/java/com/example/medrecord/SignInActivity.java
+++ app/src/main/java/com/example/medrecord/SignInActivity.java
@@ -1,11 +1,12 @@
 package com.example.medrecord;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,6 +16,7 @@ public class SignInActivity extends AppCompatActivity {
     Button signIn;
     EditText username, password;
     String usernameText, passwordText, usernameTRUE, passwordTRUE;
+    DBHelper dbHelper = new DBHelper(this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,12 +28,19 @@ public class SignInActivity extends AppCompatActivity {
         username = (EditText) findViewById(R.id.editTextUsername);
         password = (EditText) findViewById(R.id.editTextPassword);
         signIn.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void onClick(View v) {
                 usernameText = username.getText().toString();
                 passwordText = password.getText().toString();
                 if((usernameTRUE.compareTo(usernameText) == 0) && (passwordTRUE.compareTo(passwordText) == 0)){
-                    startActivity(new Intent(SignInActivity.this, MainActivity.class));
+                    //check if db is empty first.
+                    if(dbHelper.getPatientList().isEmpty()){
+                        startActivity(new Intent(SignInActivity.this, EmptyRecordActivity.class));
+                    }
+                    else {
+                        startActivity(new Intent(SignInActivity.this, RecordManagerActivity.class));
+                    }
                 }
                 else{
 
