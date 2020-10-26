cec62e46b149b2e92e98fa1be8f4bb06cf2c4031
Olubunmi Alegbeleye <olubunmialegbeleye@users.noreply.github.com>
2020-10-26 10:03:59 +0100

    Refactor MainActivity.java to EmptyRecordActivity.java

--- app/src/main/java/com/example/medrecord/EmptyRecordActivity.java
+++ app/src/main/java/com/example/medrecord/EmptyRecordActivity.java
@@ -1,21 +1,21 @@
 package com.example.medrecord;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class EmptyRecordActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_empty_record);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -23,8 +23,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Add Intent", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(new Intent(EmptyRecordActivity.this, NewRecordActivity.class));
             }
         });
     }
