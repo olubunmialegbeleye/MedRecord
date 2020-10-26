1676794badfea853df2d8e9e300dfb3df800c7c7
Jesutoby <adetoyane@gmail.com>
2020-10-26 04:05:51 +0100

    Added View Record and Health info fragment layouts

---
+++ app/src/main/java/com/example/medrecord/ViewRecordActivity.java
@@ -0,0 +1,13 @@
+package com.example.medrecord;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewRecordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_record);
+    }
+}
